16bf36ef785271f77e6be8e00d37cd487769ecc9
Glauber Costa <glommer@openvz.org>
2012-11-16 16:25:31 +0400

    cpuacct: don't actually do anything.
    All the information we have that is needed for cpuusage (and
    cpuusage_percpu) is present in schedstats. It is already recorded
    in a sane hierarchical way.
    
    If we have CONFIG_SCHEDSTATS, we don't really need to do any extra
    work. All former functions become empty inlines.
    
    Signed-off-by: Glauber Costa <glommer@openvz.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Michal Hocko <mhocko@suse.cz>
    Cc: Kay Sievers <kay.sievers@vrfy.org>
    Cc: Lennart Poettering <mzxreary@0pointer.de>
    Cc: Dave Jones <davej@redhat.com>
    Cc: Ben Hutchings <ben@decadent.org.uk>
    Cc: Paul Turner <pjt@google.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -700,8 +700,6 @@ static inline void set_task_rq(struct task_struct *p, unsigned int cpu)
 #endif
 }
 
-extern void task_group_charge(struct task_struct *tsk, u64 cputime);
-
 #else /* CONFIG_CGROUP_SCHED */
 
 static inline void set_task_rq(struct task_struct *p, unsigned int cpu) { }
@@ -709,9 +707,13 @@ static inline struct task_group *task_group(struct task_struct *p)
 {
 	return NULL;
 }
-static inline void task_group_charge(struct task_struct *tsk, u64 cputime) { }
-
 #endif /* CONFIG_CGROUP_SCHED */
+
+#if defined(CONFIG_CGROUP_SCHED) && !defined(CONFIG_SCHEDSTATS)
+extern void task_group_charge(struct task_struct *tsk, u64 cputime);
+#else
+static inline void task_group_charge(struct task_struct *tsk, u64 cputime) {}
+#endif
 
 static inline void __set_task_cpu(struct task_struct *p, unsigned int cpu)
 {
